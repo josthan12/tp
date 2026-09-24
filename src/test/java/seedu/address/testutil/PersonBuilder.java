49a076431d641bcae080f5aae0e8fb43f795f3ae
josthan12 <josthan2004wong@gmail.com>
2026-09-24 16:17:59 +0800

    Fix style

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -102,7 +102,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address,remark, tags);
+        return new Person(name, phone, email, address, remark, tags);
     }
 
 }
